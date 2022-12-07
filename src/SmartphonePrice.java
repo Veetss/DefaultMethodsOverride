545855991c9adf13dd97ce3dd8e96c62814f7fd3
Vittoria Vassarotti <112390959+Veetss@users.noreply.github.com>
2022-12-07 11:39:29 +0100

    Update SmartphonePrice.java

--- src/SmartphonePrice.java
+++ src/SmartphonePrice.java
@@ -1,2 +1,31 @@
-package PACKAGE_NAME;public class SmartphonePrice {
+public class SmartphonePrice implements Cloneable{
+
+    public String priceType;
+    public double priceInEuros;
+
+    @Override
+    public SmartphonePrice clone() throws CloneNotSupportedException {
+
+        SmartphonePrice clonedSmartphonePrice = (SmartphonePrice)super.clone();
+
+        return clonedSmartphonePrice;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        SmartphonePrice that = (SmartphonePrice) o;
+        return Double.compare(that.priceInEuros, priceInEuros) == 0 && priceType.equals(that.priceType);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(priceType, priceInEuros);
+    }
+
+    public SmartphonePrice(String type, double price){
+        this.priceType = type;
+        this.priceInEuros = price;
+    }
 }
