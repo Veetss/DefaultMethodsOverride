c743246d203b9e2578b09d0e4628dfb0bc3a8070
Vittoria Vassarotti <112390959+Veetss@users.noreply.github.com>
2022-12-07 11:38:48 +0100

    Update Smartphone.java

--- src/Smartphone.java
+++ src/Smartphone.java
@@ -1,2 +1,53 @@
-package PACKAGE_NAME;public class Smartphone {
+import java.util.Objects;
+
+public class Smartphone implements  Cloneable{
+
+    public String brandName;
+    public String modelName;
+    public int batterymAh;
+
+    public SmartphonePrice producerPrice;
+    public SmartphonePrice retailPrice;
+    public Smartphone(String brand, String model, int battery, SmartphonePrice prodP, SmartphonePrice retailP){
+        this.brandName = brand;
+        this.modelName = model;
+        this.batterymAh = battery;
+        this.producerPrice = prodP;
+        this.retailPrice = retailP;
+    }
+    @Override
+    public String toString() {
+        return "Smartphone{" +
+                "brandName='" + brandName + '\'' +
+                ", modelName='" + modelName + '\'' +
+                ", batterymAh=" + batterymAh +
+                ", producerPrice=" + producerPrice +
+                ", retailPrice=" + retailPrice +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Smartphone that = (Smartphone) o;
+        return batterymAh == that.batterymAh && brandName.equals(that.brandName) && modelName.equals(that.modelName) && producerPrice.equals(that.producerPrice) && retailPrice.equals(that.retailPrice);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(brandName, modelName, batterymAh, producerPrice, retailPrice);
+    }
+
+
+    @Override
+    public Smartphone clone() throws CloneNotSupportedException {
+
+        Smartphone clonedSmartphone = (Smartphone)super.clone();
+
+        clonedSmartphone.producerPrice = this.producerPrice;
+        clonedSmartphone.retailPrice = this.retailPrice;
+
+        return clonedSmartphone;
+    }
 }
